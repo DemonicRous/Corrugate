748e263709a7015106ecfc11dd9b634044d87017
DemonicRous <asashachernov@mail.ru>
2025-09-11 14:22:03 +0500

    Init...

---
+++ app/Models/User.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    protected $attributes = [
+        'role' => 'user', // Устанавливаем значение по умолчанию 'user' для поля role
+    ];
+
+    /**
+     * Константы ролей для удобства использования
+     */
+    const ROLE_USER = 'user';
+    const ROLE_ADMIN = 'admin';
+
+    /**
+     * Получить все возможные роли
+     *
+     * @return array
+     */
+    public static function getRoles()
+    {
+        return [
+            self::ROLE_USER,
+            self::ROLE_ADMIN,
+        ];
+    }
+
+    /**
+     * Проверить, является ли пользователь администратором
+     *
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role === self::ROLE_ADMIN;
+    }
+}
